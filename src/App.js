ea9ed766978125b47f357fad48eb522789652fad
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-05-06 17:34:58 +0200

    Display correct trips

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Countries from './components/views/Countries/CountriesContainer';
 import Regions from './components/views/Regions/RegionsContainer';
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
+import Country from './components/views/Country/CountryContainer';
+import Trip from './components/views/Trip/TripContainer';
 
 import parseTrips from './utils/parseTrips';
 import {setMultipleStates} from './redux/globalRedux';
@@ -46,6 +48,8 @@ class App extends React.Component {
             <Route exact path='/countries' component={Countries}/>
             <Route exact path='/regions' component={Regions}/>
             <Route exact path='/info' component={Info} />
+            <Route exact path='/country/:id' component={Country}/>
+            <Route exact path='/trip/:id' component={Trip}/>
             <Route path='*' component={NotFound} />
           </Switch>
         </MainLayout>
